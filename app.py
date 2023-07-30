7464dd3d599feb5d5d58668dee35e27df83b60d5
Ritesh Ranjan <riteshranjan1620@gmail.com>
2023-07-31 03:00:39 +0530

    added requirements

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Choose Discipline!'
+
+@app.route('/discipline')
+def discipline():
+    # write a motivational quote
+    return 'Discipline is the bridge between goals and accomplishment. - Jim Rohn'
+
+if __name__ == '__main__':
+    app.run()
